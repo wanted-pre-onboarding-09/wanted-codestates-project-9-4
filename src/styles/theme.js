8132329150173fd1c8fe9c40f5876cc5af7a14b5
y-solb <ysolb99@gmail.com>
2022-03-01 00:27:27 +0900

    Feat: Add ThemeProvider

---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+const colors = {
+  white: '#fff',
+  black: '#000',
+  main1: '#5e72e4',
+  main2: '#669cff',
+  gray1: '#c4c4c4',
+  gray2: '#979797',
+  green: '#2dce89',
+  puple: '#9695D7',
+  red: '#c4302b',
+};
+
+const theme = { colors };
+export default theme;
