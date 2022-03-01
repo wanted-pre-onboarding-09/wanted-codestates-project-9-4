580e2ded9809d623ef161ec1e69a68460d4098b3
y-solb <ysolb99@gmail.com>
2022-03-01 19:53:34 +0900

    Feat: Add MainSlider component

---
+++ src/components/organisms/MainSlider.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Slider from '../molecules/Slider';
+import Title from '../molecules/Title';
+
+function MainSlider() {
+  return (
+    <>
+      <Title text="새로 올라왔어요" tab="NEW" />
+      <Slider />
+    </>
+  );
+}
+
+export default MainSlider;
