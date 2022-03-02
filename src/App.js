8186bce9bc1e7149a150c619d83ded3c5e9376a9
y-solb <ysolb99@gmail.com>
2022-03-02 17:37:19 +0900

    Chore: Change Header name

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Detail from './components/pages/Detail';
-import Header from './components/organisms/header';
+import Header from './components/organisms/Header';
 import ScrollToTop from './components/molecules/ScrollTop';
 
 localStorage.clear(); /* refresh 일 경우 localStorage 모두 삭제 */
