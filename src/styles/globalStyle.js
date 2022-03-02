5ca719c6ae3d7726c99f7b4b1224515a430c231e
y-solb <ysolb99@gmail.com>
2022-03-02 16:09:36 +0900

    Design: Add globalStyle

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -3,11 +3,21 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   *, *::before, *::after {
     box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font: inherit;
+    vertical-align: baseline;
   }
 
   body {
-    
-  }
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
 
 ul{
     list-style: none;
@@ -21,6 +31,25 @@ li {
 button{
     cursor: pointer;
     outline: 0;
+}
+
+:root{
+  font-size: 16px;
+}
+@media screen and (max-width: 768px) {
+  :root {
+    font-size: 14px;
+  }
+}
+@media screen and (max-width: 425px) {
+  :root {
+    font-size: 12px;
+  }
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
 }
 
 `;
