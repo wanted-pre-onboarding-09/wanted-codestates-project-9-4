48d4206ab6318c548d601ee5f452cddb558f7bc0
y-solb <ysolb99@gmail.com>
2022-03-02 17:38:33 +0900

    Design: Change margin

--- src/components/atoms/SliderDots.js
+++ src/components/atoms/SliderDots.js
@@ -6,10 +6,10 @@ const DotList = styled.ul`
   display: flex;
   justify-content: center;
   width: 100%;
-  margin-top: 2.5rem;
+  margin-top: 3.5rem;
   @media screen and (max-width: 768px) {
     justify-content: flex-start;
-    margin-top: 5rem;
+    margin: 2.5rem 0 0 0.6rem;
   }
 `;
 
